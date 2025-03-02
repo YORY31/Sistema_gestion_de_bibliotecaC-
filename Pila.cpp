6c9017ecc5c6fa79ec9faf69310abfb050b0b9a2
YORY31 <20230272@itla.edu.do>
2025-03-01 22:36:04 -0400

    first commit

---
+++ Pila.cpp
@@ -0,0 +1,16 @@
+#include "Pila.h"
+
+Pila::Pila(): _ultimo(NULL)
+{
+}
+
+void Pila::Agregar(Elemento* elemento)
+{
+    /* Llenar este método con su implementación de Agregar elemento a la pila */
+}
+
+Elemento* Pila::Extraer()
+{
+    /* Llenar este método con su implementación de Extraer un elemento de la pila */
+    return NULL;
+}
